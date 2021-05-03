9c4df1c23cb68a0c0bc0af98749cce160e115488
E-Weber <ewebersms@gmail.com>
2021-05-03 11:47:25 -0500

    slight progress

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 
-function buildPlot(sample) {
+function buildPlot(id) {
+
+    //grab json data
     d3.json("samples.json").then((data) => {
         console.log(data);
 
+        // metadata
         var metadata = data.metadata;
         console.log(metadata);
 
-        var filterData = metadata.filter(filtered => filtered.id == sample);
-        console.log(filterData[0]);
-    });
+        // var wfreq w map
+        var wfreq = data.metadata.map(d => d.wfreq);
+        console.log({ wfreq });
+
+        // filter the sample data by id
+        var sampleData = data.sampleData.filter(s => s.id.toString() === id[0]);
+        console.log(sampleData);
+
+        // grab top 10
+        var sliceSample = sampleData.sample_values.slice(0, 10).reverse();
+    })
 };
 
 buildPlot(1);
